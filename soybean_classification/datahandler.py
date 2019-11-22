f8ebde6f5a17b4ec289a31d981f832be01e62e5e
CSerxzm <3052720966@qq.com>
2019-11-22 23:35:08 +0800

    添加文件

---
+++ soybean_classification/datahandler.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy
+from sklearn.preprocessing import Imputer
+
+import soybean_classification
+
+
+def load_data_set():
+    url = "../data/soybean-large.data"
+    names = ['Class', 'date', 'plant-stand', 'precip', 'temp', 'hail', 'crop-hist', 'area-damaged',
+             'severity', 'seed-tmt', 'germination', 'plant-growth',
+             'leaves', 'leafspots-halo', 'leafspots-marg', 'leafspot-size', 'leaf-shread', 'leaf-malf',
+             'leaf-mild', 'stem', 'lodging', 'stem-cankers', 'canker-lesion', 'fruiting-bodies',
+             'external decay', 'mycelium', 'int-discolor', 'sclerotia', 'fruit-pods', 'fruit spots',
+             'seed', 'mold-growth', 'seed-discolor', 'seed-size', 'shriveling', 'roots']
+    dataset = pd.read_csv(url, names=names)
+    dataset = dataset.replace({'?':numpy.nan}) 
+    df1=dataset.iloc[:, 1:]
+    df2=dataset.iloc[:, :1]
+    imr= Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0 )
+    imr = imr.fit(df1)
+    imputed_data = imr.transform(df1.values)
+    df = pd.DataFrame(imputed_data) 
+    df=pd.concat([df2,df],axis=1)
+    return df
