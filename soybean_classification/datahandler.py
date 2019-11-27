6aed35b74eb1af6b5067c934cfb810066ca8c062
CSerxzm <3052720966@qq.com>
2019-11-27 20:00:55 +0800

    众数填充

--- soybean_classification/datahandler.py
+++ soybean_classification/datahandler.py
@@ -41,7 +41,7 @@ def load_data_set_test(df_col_mean):
     
     df1=dataset.iloc[:, 1:]
     df2=dataset.iloc[:, :1]
-    imr= Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0 )
+    imr= Imputer(missing_values = 'NaN', strategy = 'most_frequent', axis = 0 )
     imr = imr.fit(df1)
     imputed_data = imr.transform(df1.values)
     df = pd.DataFrame(imputed_data) 
